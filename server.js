52ab13ec693dcd4aa80e38769ec2df770c62abb2
89209062+dcrlsn@users.noreply.github.com <c4r150n@gmail.com>
2021-12-19 13:14:50 -0500

    post requests

--- server.js
+++ server.js
@@ -1,4 +1,28 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs')
-const uuid = require('uuid');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+app.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, "/db/db.json"))
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+
+app.listen(PORT, () =>
+  console.log(`App @ http://localhost:${PORT} 🚀🌑`)
+);
